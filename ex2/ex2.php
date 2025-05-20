3304ba34ff8b631fa71bcbc2dcbd9344f5a66f6c
DaniloGomes7 <danilo.gomes48@etec.sp.gov.br>
2025-05-19 22:18:27 -0300

    ex

---
+++ ex2/ex2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex2</title>
+</head>
+<body>
+    <form method="POST">
+
+        Informe um nome de usuario:  <input type="text" name="nome" required>
+        Informe uma senha : <input type="text" name="senha" required>
+        <br>
+        <input type="submit" value="Verificar" /><br>
+
+    </form>
+</body>
+</html>
